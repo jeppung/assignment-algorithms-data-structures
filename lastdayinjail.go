9c880962b872102ff7ab71bcce40f1fd33aca047
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-08-23 21:11:15 +0700

    Adding excercise1 code, modifying sort algorithm in excercise1

--- lastdayinjail.go
+++ lastdayinjail.go
@@ -10,9 +10,31 @@ type Person struct {
 // Task 1.a
 func LastDayInJail(criminals []Person, chosenPerson string) (onTransport []Person, waiting []Person) {
 	// Write your code here
-	bubbleSort(criminals)
+	released := []Person{}
+
+	sort(criminals)
+
+	if len(criminals) >= 5 {
+		released = append(released, criminals[:5]...)
+	} else {
+		released = append(released, criminals...)
+	}
+
+	chosenPersonIndex := searchChosenPerson(criminals, chosenPerson)
+	if (chosenPerson != "") && (chosenPersonIndex != -1) {
+		released = append(released, criminals[chosenPersonIndex])
+	}
+
+	if len(released) >= 3 {
+		onTransport = append(onTransport, criminals[:3]...)
+		waiting = append(waiting, released[3:]...)
+	} else {
+		onTransport = append(onTransport, criminals...)
+		waiting = append(waiting, []Person{}...)
+	}
+
 	// --------------------
-	return nil, nil
+	return onTransport, waiting
 	// --------------------
 }
 
@@ -25,14 +47,21 @@ func searchChosenPerson(criminals []Person, chosenPerson string) int {
 	return -1
 }
 
-func bubbleSort(criminals []Person) {
+func sort(criminals []Person) {
 	swapped := false
 
 	for {
 		for i := 0; i < len(criminals)-1; i++ {
-			if criminals[i].CriminalScore > criminals[i+1].CriminalScore || criminals[i].Name > criminals[i+1].Name {
-				criminals[i], criminals[i+1] = criminals[i+1], criminals[i]
+			if criminals[i].CriminalScore > criminals[i+1].CriminalScore {
+				temp := criminals[i]
+				criminals[i] = criminals[i+1]
+				criminals[i+1] = temp
 				swapped = true
+			} else if criminals[i].CriminalScore == criminals[i+1].CriminalScore && criminals[i].Name > criminals[i+1].Name {
+				temp := criminals[i]
+				criminals[i] = criminals[i+1]
+				criminals[i+1] = temp
+				swapped = false
 			} else {
 				swapped = false
 			}
